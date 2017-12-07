95d5459499d73fb15b283d3afeffc6d7d871f47f
Frank Brückner <dev@froschdesignstudio.de>
2017-12-07 22:45:25 +0100

    Updates JavaScript files

--- asset/js/base.js
+++ asset/js/base.js
@@ -1,19 +1,32 @@
-/* Highlight */
-$(function () {
-    $('table').addClass('table table-striped table-hover');
-});
-
-$('body').scrollspy({
-    target: '.bs-sidebar'
-});
-
-/* Prevent disabled links from causing a page reload */
-$("li.disabled a").click(function (e) {
-    e.preventDefault();
-});
-
 (function () {
     "use strict";
+
+    // Tables
+    var tables = document.querySelectorAll('.content tables');
+    if (tables.length) {
+        tables.classList.add('table table-striped table-hover');
+    }
+
+    // Anchors
     anchors.options.placement = 'left';
-    anchors.add('.docs h1, .docs h2, .docs h3, .docs h4, .docs h5');
+    anchors.add(
+        '.content h1:not(.content__title), .content h2, .content > h3, .content h4, .content h5'
+    );
+
+    var shiftWindow = function () {
+        scrollBy(0, -50)
+    };
+    if (location.hash) {
+        shiftWindow();
+    }
+    window.addEventListener('hashchange', shiftWindow);
+
+    // Remove fixed position if subnavigation is bigger than content
+    var contentElement       = document.querySelector('.content');
+    var subnavigationElement = document.querySelector('.subnavigation');
+    if (contentElement && subnavigationElement
+        && contentElement.clientHeight < subnavigationElement.clientHeight
+    ) {
+        subnavigationElement.classList.remove('affix')
+    }
 })();
