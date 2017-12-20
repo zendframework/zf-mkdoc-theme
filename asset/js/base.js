21ecf8f271025f2d46bd2da476c169900f2f9ed1
Frank Brückner <dev@froschdesignstudio.de>
2017-12-20 22:53:52 +0100

    Adds JavaScript code for missing line-numbers class

--- asset/js/base.js
+++ asset/js/base.js
@@ -15,6 +15,12 @@
         '.content h1:not(.content__title), .content h2, .content > h3, .content h4, .content h5'
     );
 
+    // Pre elements
+    var preElements = document.querySelectorAll('pre');
+    preElements.forEach(function (element) {
+        element.classList.add('line-numbers');
+    });
+
     // Search modal
     $('#mkdocs_search_modal').on('shown.bs.modal', function () {
         $('#mkdocs-search-query').focus();
