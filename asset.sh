abdf7f0fda9d50fbc7bbc28c6b5d197f93327e5d
Matthew Weier O'Phinney <matthew@zend.com>
2019-04-17 17:53:56 -0500

    fix: use block within asset script
    cd within a block to ensure that the build happens in the correct
    directory

--- asset.sh
+++ asset.sh
@@ -3,14 +3,16 @@
 
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
 
-cd $SCRIPT_PATH/asset
-npm install --no-save
+(
+    cd $SCRIPT_PATH/asset ;
+    npm install --no-save ;
 
-# Running each task separately due to concurrency issues when run
-# at once via `gulp`
-gulp images
-gulp icons
-gulp scripts
-gulp styles
+    # Running each task separately due to concurrency issues when run
+    # at once via `gulp`
+    gulp images ;
+    gulp icons ;
+    gulp scripts ;
+    gulp styles ;
 
-perl -pi -e 's/\.[a-z]+"(:)|"|,|^\{\n$|\}/$1/g' ../assets.yml
+    perl -pi -e 's/\.[a-z]+"(:)|"|,|^\{\n$|\}/$1/g' ../assets.yml ;
+)
