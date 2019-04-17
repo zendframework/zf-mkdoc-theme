8ceb7b2d29a35990408369dfa38fa9906c726c44
Matthew Weier O'Phinney <matthew@zend.com>
2019-04-17 18:43:25 -0500

    fix: Remove package-lock.json before install
    This is done to remove potential errors due to npm version mismatches.

--- asset.sh
+++ asset.sh
@@ -3,16 +3,15 @@
 
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
 
-(
-    cd $SCRIPT_PATH/asset ;
-    npm install --no-save ;
+cd $SCRIPT_PATH/asset
+rm package-lock.json
+npm install --no-save
 
-    # Running each task separately due to concurrency issues when run
-    # at once via `gulp`
-    gulp images ;
-    gulp icons ;
-    gulp scripts ;
-    gulp styles ;
+# Running each task separately due to concurrency issues when run
+# at once via `gulp`
+gulp images
+gulp icons
+gulp scripts
+gulp styles
 
-    perl -pi -e 's/\.[a-z]+"(:)|"|,|^\{\n$|\}/$1/g' ../assets.yml ;
-)
+perl -pi -e 's/\.[a-z]+"(:)|"|,|^\{\n$|\}/$1/g' ../assets.yml
